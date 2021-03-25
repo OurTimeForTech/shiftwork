5bcb21ff50f222a6627686ef503735a2fe5ecc78
ncbui <ncbui@users.noreply.github.com>
2021-03-24 22:42:36 -0700

    requires org fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_175702) do
+ActiveRecord::Schema.define(version: 2021_03_25_053812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "org_name", null: false
+    t.string "org_address", null: false
+    t.string "org_city", null: false
+    t.string "org_state", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "shifts", force: :cascade do |t|
     t.boolean "shift_status"
@@ -23,6 +32,18 @@ ActiveRecord::Schema.define(version: 2021_03_17_175702) do
     t.integer "shift_pay"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
