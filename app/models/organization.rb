00e2676b0aab2cfa038f1d1e678c821e2db506d2
ncbui <ncbui@users.noreply.github.com>
2021-03-24 22:45:40 -0700

    Added validation to form

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,6 @@
 class Organization < ApplicationRecord
+    validates :org_name, :presence => true
+    validates :org_address, :presence => true
+    validates :org_city, :presence => true
+    validates :org_state, :presence => true
 end
