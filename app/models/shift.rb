b9486f9e5757c924d123da9f34bf0b3c20d73f1c
ncbui <ncbui@users.noreply.github.com>
2021-04-01 12:49:18 -0700

    Added validation fto check that pay rate is greater than or equal to 0

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,14 +1,15 @@
 class Shift < ApplicationRecord
-    validates :role_name, :presence => true
-    validates :shift_start, :presence => true
-    validates :shift_end , :presence => true
-    validates :shift_pay, :presence => true
+  validates :role_name, :presence => true
+  validates :shift_start, :presence => true
+  validates :shift_end , :presence => true
+  validates :shift_pay, :presence => true
+  validates_numericality_of :shift_pay, greater_than_or_equal_to: 0.00
   
-   validate :end_date_after_start_date?
+  validate :end_date_after_start_date?
 
-   def end_date_after_start_date?
-       if shift_end < shift_start
-          errors.add :shift_end, "shift must be after shift start"
-       end
+  def end_date_after_start_date?
+    if shift_end < shift_start
+      errors.add :shift_end, "shift must end after shift start"
     end
+  end
 end
