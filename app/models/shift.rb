3a59b2d8f75ce4bd30fa57f699bdffc554c55e9c
ncbui <ncbui@users.noreply.github.com>
2021-04-02 16:08:34 -0700

    Add cvalidation to check for shifts that start in the past

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -6,10 +6,17 @@ class Shift < ApplicationRecord
   validates_numericality_of :shift_pay, greater_than: 0.00
   
   validate :end_date_after_start_date?
+  validate :shift_start_before_current_date?
 
   def end_date_after_start_date?
     if shift_end < shift_start + 3600
       errors.add :shift_end, "shift must end at least an hour after shift start"
+    end
+  end
+
+  def shift_start_before_current_date?
+    if shift_start < Time.zone.now
+      errors.add :shift_start, "shift cannot start in the past" 
     end
   end
 end
